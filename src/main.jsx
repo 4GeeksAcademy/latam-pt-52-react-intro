0039c393109982760a34f25036747576a6c5b22c
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2025-06-19 03:55:06 +0000

    :tada: Our first component

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import Navbar from "./Navbar.jsx" // default
+
+import { Footer } from "./Footer.jsx" // simple export
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
+    <Footer/>
   </StrictMode>,
 )
