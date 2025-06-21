a8557c5f6abacaf793ade6d86d86f56049663b24
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2025-06-21 02:28:52 +0000

    :zap: Replicated DOM project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
+import { useState } from 'react';
+
 
 function App() {
- 
+
+  const randomElement = (arr) => arr[Math.floor(Math.random()*arr.length)]
+
+  const generateRandomColor = () => {
+    const hexValues = "0123456789ABCDEF";
+    let newColor = '#';
+    for (let i = 0; i < 6; i++) {
+      newColor = newColor + randomElement(hexValues);
+    }
+    return newColor;
+  }
+
+  // todos los ganchos o supeFunciones van dentro del componente
+  const [listaColores, setListaColors] = useState([generateRandomColor()])
+
+  const addColor = (event) => {
+
+    const posicionHorizontalCursor = event.pageX;
+    const anchoDelElemento = event.target.offsetWidth
+    if (posicionHorizontalCursor > (anchoDelElemento) / 2) {
+      
+      setListaColors([ ...listaColores, generateRandomColor() ])
+      return;
+    }
+    
+    const colorAEliminar = randomElement(listaColores);
+    setListaColors(listaColores.filter( color => color != colorAEliminar))
+  }
+  
   return (
     <>
-      <div className="flex w-full ">
-        <h1 className="text-6xl font-bold mx-auto" >Hello latam pt 52 ❤️</h1>
+      <div className="flex flex-col justify-center align-center w-full min-h-[90vh] overflow-y-scroll"
+        onClick={(event) => addColor(event)}
+      >
+        {
+          listaColores.map(color => (
+            <div className="h-32 w-32 mx-auto text-center content-center font-bold" style={{ backgroundColor: color }}> 
+              {color}
+            </div>
+          ))
+        }
+
       </div>
     </>
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,12 +22,12 @@ const Navbar = () => (
             <img className="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
             </div>
             <div className="hidden sm:ml-6 sm:block">
-            <div className="flex space-x-4">
+            {/* <div className="flex space-x-4">
                 <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" ariaCurrent="page">Dashboard</a>
                 <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
                 <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
                 <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-            </div>
+            </div> */}
             </div>
         </div>
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -48,11 +48,11 @@ const Navbar = () => (
                 </button>
             </div>
 
-            <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" ariaOrientation="vertical" ariaLabelledby="user-menu-button" tabindex="-1">
+            {/* <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" ariaOrientation="vertical" ariaLabelledby="user-menu-button" tabindex="-1">
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-            </div>
+            </div> */}
             </div>
         </div>
         </div>
